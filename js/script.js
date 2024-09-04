b2a158a05183a22caf2d1997f02c81cfcf66786a
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 <11ahmedg159@gmail.com>
2024-09-04 18:21:36 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-btnChek = document.getElementById("btn-check")
+navitem = document.getElementById("nav-link");
+btnChek = document.getElementById("btn-check");
+
+
+/////////////////////////////////////Hedear////////////////////////////////////////
+
+navitem.addEventListener("click", function() {
+    navitem.addC
+})
 
 btnChek.addEventListener("click", function() {
     window.open("../img/certificatepdf.pdf", "_blank");
